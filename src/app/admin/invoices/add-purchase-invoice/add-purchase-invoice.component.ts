73066ec83eac03be7aca19e4dd1ea9aa25b89074
MonaSoliman009 <monasoliman009@gmail.com>
2021-09-15 20:07:23 +0200

    changes

--- src/app/admin/invoices/add-purchase-invoice/add-purchase-invoice.component.ts
+++ src/app/admin/invoices/add-purchase-invoice/add-purchase-invoice.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPurchaseInvoiceComponent implements OnInit {
   newSection:any=[0];
-
+  newSection2:any=[0]
   constructor() { }
 
   ngOnInit(): void {
   }
   addNewSection(){
     this.newSection.push(2)
+  }
+  addNewSection2(){
+    this.newSection2.push(2)
   }
 }
