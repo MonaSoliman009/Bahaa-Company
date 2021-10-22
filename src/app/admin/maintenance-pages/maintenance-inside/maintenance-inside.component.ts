b0f210fe5f98206420a55bdbdb9b3028e25cc3bf
MonaSoliman009 <monasoliman009@gmail.com>
2021-10-23 01:57:29 +0200

    updates

--- src/app/admin/maintenance-pages/maintenance-inside/maintenance-inside.component.ts
+++ src/app/admin/maintenance-pages/maintenance-inside/maintenance-inside.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl,FormArray, FormGroup, Validators } from '@angular/forms';
 import { MaintenanceService } from '../../services/maintenance.service';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-maintenance-inside',
@@ -13,42 +14,106 @@ export class MaintenanceInsideComponent implements OnInit {
   newSection: any = [0];
   newPart: any = [0];
   form: FormGroup;
-i:number=0;
+ 
+i:number=1;
   submitted: boolean;
   submitted2: boolean;
-
+  displayRecomendedSerial = true;
+  allSerialNumbers = new Array();
+  Serial: any;
   MaintenanceInsideForm: FormGroup;
+  products: any;
+  @ViewChild('Serial') serial: ElementRef;
 
-  constructor(private _fb: FormBuilder, public _MaintenanceService: MaintenanceService) { }
+  constructor(private _fb: FormBuilder, public _MaintenanceService: MaintenanceService,private _ProductService: ProductService) { }
 
   ngOnInit(): void {
     this.creteForm();
-    this.creteMaintenanceInsideForm()
+    this.creteMaintenanceInsideForm();
+    this.getAllproducts();
   }
 
   addNewSection() {
-    this.newSection.push(2)
+    this.MaintenanceData.push(this.newMaintenanceData())
+
+    this.newSection.push(2);
+
   }
   addNewPart() {
-    this.i++;
+    this.sparePartsData.push(this.newsparePartsData());
+  
    
-    this.newPart.push(this.i)
+    this.newPart.push(1)
     console.log(this.newPart)
 
   }
+  onItemChange(value){
+    for(var i=this.MaintenanceData.value.length;i>0;i--){
+      this.MaintenanceData.removeAt(i-1)
+
+    }
+    for(var i=this.sparePartsData.value.length;i>0;i--){
+      this.sparePartsData.removeAt(i-1)
+  
+    }
+    this.MaintenanceData.push(this.newMaintenanceData())
+
+    console.log(" Value is : ", this.MaintenanceData.value );
+ }
+ onItemChange2(value){
+  for(var i=this.sparePartsData.value.length;i>0;i--){
+    this.sparePartsData.removeAt(i-1)
+
+  }
+  for(var i=this.MaintenanceData.value.length;i>0;i--){
+    this.MaintenanceData.removeAt(i-1)
+
+  }
+  this.sparePartsData.push(this.newsparePartsData())
+
+  console.log(" Value is : ", this.sparePartsData.value );
+}
   creteForm() {
     this.form = this._fb.group({
       serialNumber: ["", [Validators.required]],
     });
   }
   creteMaintenanceInsideForm() {
-    this.MaintenanceInsideForm = new FormGroup({
-      isAdded: new FormControl('',Validators.required),
-      Existence: new FormControl(''),
-      inside:new FormControl(''),
-      repaired:new FormControl('',Validators.required)
+    this.MaintenanceInsideForm =  this._fb.group({
+      isAdded: ['',[Validators.required]],
+       Existence: [''],
+      sparePartsData: this._fb.array([]) ,
+      MaintenanceData:this._fb.array([]) ,
+      // Existence: new FormControl(''),
+      // inside:new FormControl(''),
+      repaired:['']
     });
+    // this.sparePartsData.push(this.newsparePartsData())
+    // this.MaintenanceData.push(this.newMaintenanceData())
+
   }
+  
+get sparePartsData() : FormArray {
+  return this.MaintenanceInsideForm.get("sparePartsData") as FormArray
+}
+get MaintenanceData() : FormArray {
+  return this.MaintenanceInsideForm.get("MaintenanceData") as FormArray
+}
+newsparePartsData(): FormGroup {
+  return this._fb.group({
+    serialNumber: [''],
+  
+      isInside:[''],
+      product:['']
+  
+  })
+}
+newMaintenanceData(): FormGroup {
+  return this._fb.group({
+    
+    sparePartNumber:['']
+  })
+}
   alertWithFail(msg) {
     Swal.fire('Failed', msg, 'error').then(
       (res) => {
@@ -61,7 +126,7 @@ i:number=0;
     this.started = true;
 
     // if (this.form.valid) {
-    //   this._MaintenanceService.startMaitenanceInsideStore(this.form.value.serialNumber, localStorage.getItem("emp")).subscribe((res: any) => {
+    //   this._MaintenanceService.startMaitenanceInsideStore(this.Getserial(), localStorage.getItem("id")).subscribe((res: any) => {
     //     console.log(res)
     //     if (res.message == 'Maintainence Started successfully') {
     //       this.started = true;
@@ -71,7 +136,7 @@ i:number=0;
 
     //     }
     //   }, (err) => {
-    //     console.log(err)
+    //     this.alertWithFail(err);
 
     //   })
     //   console.log(this.form.value.serialNumber, localStorage.getItem("emp"))
@@ -79,8 +144,42 @@ i:number=0;
   }
   onSubmit(){
    this.submitted2=true;
-   if(this.MaintenanceInsideForm.valid){
-     console.log(this.MaintenanceInsideForm.value)
-   }
+        console.log(this.MaintenanceInsideForm.value)
+
+  //  if(this.MaintenanceInsideForm.valid){
+  //    console.log(this.MaintenanceInsideForm.value)
+  //  }
+  }
+  getAllproducts() {
+    this._ProductService.getAllProduct().subscribe((res) => {
+      this.products = res;
+      for (let val of this.products) {
+        let stringSerial = val.serialNumber.toString();
+        console.log(stringSerial);
+
+        this.allSerialNumbers.push(val.serialNumber.toString());
+      }
+    });
+  }
+  selectedSerial(val) {
+    this.serial.nativeElement.value = val;
+    this.displayRecomendedSerial = true;
+    // this.form.controls.serialNumber.valid;
+
+  }
+  data(val) {
+    return val;
+  }
+  searchSerial(val) {
+    this.displayRecomendedSerial = false;
+    
+
+    let filterd = this.allSerialNumbers.filter(this.data);
+
+  }
+  Getserial() {
+    const valueInput = this.serial.nativeElement.value;
+    this.Serial = valueInput;
+    return valueInput;
   }
 }
